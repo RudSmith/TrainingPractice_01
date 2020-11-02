3fc6ef64b8080a1dfcc7caf95afd8250dc53cd4e
RudSmith <igor200rudenskiy@gmail.com>
2020-11-02 11:22:53 +0300

    Solved task 1

--- RIM_Task_01/RIM_Task_01.cs
+++ RIM_Task_01/RIM_Task_01.cs
@@ -10,6 +10,28 @@ namespace RIM_Task_01
     {
         static void Main(string[] args)
         {
+            // Reading initial gold amount from keyboard
+            Console.WriteLine("Please, enter your amount of gold: ");
+            // TryParse() will validate the input
+            ulong.TryParse(Console.ReadLine(), out ulong userGoldAmount);
+
+            // Gold to crystalls exchange rate
+            const ulong exchangeRate = 50;
+
+            Console.WriteLine($"Today`s gold to crystalls exchange rate: 1 crystall costs { exchangeRate } gold. \n");
+
+            // Reading the amount of crystalls, that user wants to buy
+            Console.WriteLine("Please, enter amount of crystalls you want to buy: ");
+            ulong.TryParse(Console.ReadLine(), out ulong crystallsBuyingAmount);
+
+            // Crystalls amount that user has
+            ulong userCrystallsAmount = 0;
+
+            Console.WriteLine( exchangeRate * crystallsBuyingAmount > userGoldAmount 
+                ? $"Not enougn money. Your balance: { userGoldAmount } gold and { userCrystallsAmount } crystalls."
+                : $"Success. Your balance: { userGoldAmount - exchangeRate * crystallsBuyingAmount } gold and { crystallsBuyingAmount } crystalls.");
+
+            Console.ReadKey();
         }
     }
 }
