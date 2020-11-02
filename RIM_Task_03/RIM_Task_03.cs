0a97fa3269b7129e132f787e93a68b639e0c70a7
RudSmith <igor200rudenskiy@gmail.com>
2020-11-02 11:48:08 +0300

    Sovled task 3

--- RIM_Task_03/RIM_Task_03.cs
+++ RIM_Task_03/RIM_Task_03.cs
@@ -10,6 +10,36 @@ namespace RIM_Task_03
     {
         static void Main(string[] args)
         {
+            // Setting the password
+            const string password = "pantsOnHead";
+            const string secretWord = "Mug";
+            // Creating variable, that will store the amount of available password input attempts
+            uint attemptsCount = 0;
+            // Setting max amount of tries
+            const uint maxAttempts = 3;
+
+            // Repeating the loop while user still has attempts available
+            while (attemptsCount < maxAttempts)
+            {
+                Console.WriteLine("Enter the password: ");
+                string userInput = Console.ReadLine();
+
+                // Checking password correctness
+                if (userInput != password)
+                {
+                    ++attemptsCount;
+                    Console.WriteLine($"Wrong password. Try again. You have { maxAttempts - attemptsCount } attempts left. \n");
+                }
+                else
+                {
+                    Console.WriteLine($"Correct password. Secret word is { secretWord } \n");
+                    break;
+                }
+            }
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
         }
     }
 }
